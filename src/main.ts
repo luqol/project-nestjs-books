dce008c88c19cbe97137ea2fcf37c08c1a3eff43
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-06-25 09:42:19 +0200

    Init project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes( new ValidationPipe( { transform: true}));
+  app.setGlobalPrefix('app');
+  await app.enableShutdownHooks();
+  await app.listen(8000);
+}
+bootstrap();
